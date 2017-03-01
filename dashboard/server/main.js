e0efe135c718f56e896e0a238fb5ba5abaf52fb2
Johan de Jager <johan.dejager@live.nl>
2017-03-01 17:44:25 +0100

    More shit

--- dashboard/server/main.js
+++ dashboard/server/main.js
@@ -1,15 +1,29 @@
 import { Meteor } from 'meteor/meteor';
-import '../imports/api/nodes.js';
+import { Nodes} from '../imports/api/nodes.js';
 let net = require('net');
 
+let port = 1337;
 
 Meteor.startup(() => {
-    let server = net.createServer(function(socket) {
-        socket.write('Echo server\r\n');
+    let server = net.createServer(Meteor.bindEnvironment(function(socket) {
+        console.log('CONNECTED (' + socket.localPort + '): ' + socket.remoteAddress + ':' + socket.remotePort);
 
-        console.log('Got a message mate');
-        socket.pipe(socket);
+        socket.on('data', Meteor.bindEnvironment(function (data) {
+            Nodes.insert({ip: '192.168.2.' + (Math.random() % 255)});
+
+            // close connection
+            socket.end();
+        }));
+
+        socket.on('error', function (error) {
+            console.log('******* ERROR ' + error + ' *******');
+
+            // close connection
+            socket.end();
+        });
+    }));
+
+    server.listen(port, function() {
+        console.log('Listening on ' + port);
     });
-
-    server.listen(1337, '127.0.0.1');
 });
