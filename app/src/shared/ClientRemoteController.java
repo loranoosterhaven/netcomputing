98caface6b6318306828b7a4d13643da2639ba6b
Hein Rietman <heinrietman@gmail.com>
2017-04-02 12:02:09 +0200

    config files

--- app/src/shared/ClientRemoteController.java
+++ app/src/shared/ClientRemoteController.java
@@ -6,10 +6,32 @@ import java.rmi.server.ServerNotActiveException;
 
 public interface ClientRemoteController extends Remote
 {
+    /**
+     * Register a new node
+     * @return true if register succesfull, else false
+     * @throws Exception
+     */
     boolean registerNode() throws Exception;
+
+    /**
+     * Unregister a node, given that it is contained in the list
+     * @return true if unregister succesfull, else false
+     * @throws Exception
+     */
     boolean unregisterNode() throws Exception;
 
+    /**
+     * Update a client, given that it is contained in the node list
+     * @param deviceInfo the new device info
+     * @return true if update succesfull, false else
+     * @throws Exception
+     */
     boolean updateNode( DeviceInfo deviceInfo ) throws Exception;
 
+    /**
+     * Checks if a client should shutdown
+     * @return true if client should shutdown, false else
+     * @throws Exception
+     */
     boolean shouldShutdown() throws Exception;
 }
